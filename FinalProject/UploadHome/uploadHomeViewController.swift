bdfde0cf2517f0d08be9dc3f6b156faf14e50cb7
carnage08 <rocco.m.salerno@gmail.com>
2018-09-21 05:49:33 -0400

    Home upload values are stored in database

--- FinalProject/UploadHome/uploadHomeViewController.swift
+++ FinalProject/UploadHome/uploadHomeViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class uploadHomeViewController: UIViewController {
+class uploadHomeViewController: UIViewController, UITextFieldDelegate {
 
+    var firebaseReference:DatabaseReference?
     @IBOutlet weak var nameLabel: UITextField!
     @IBOutlet weak var addressLabel: UITextField!
     @IBOutlet weak var phoneNumberLabel: UITextField!
+    @IBOutlet weak var cityLabel: UITextField!
+    @IBOutlet weak var stateLabel: UITextField!
+    @IBOutlet weak var zipcodeLabel: UITextField!
+    
     @IBOutlet weak var homeTypeOutlet: UIPickerView!
     @IBOutlet weak var homeTypeFromPicker: UITextField!
     
@@ -20,7 +26,14 @@ class uploadHomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        firebaseReference = Database.database().reference()
         
+        self.nameLabel.delegate = self
+        self.addressLabel.delegate = self
+        self.cityLabel.delegate = self
+        self.stateLabel.delegate = self
+        self.zipcodeLabel.delegate = self
+        self.phoneNumberLabel.delegate = self
         homeTypeOutlet.dataSource = self
         homeTypeOutlet.delegate = self
         // Do any additional setup after loading the view.
@@ -30,16 +43,40 @@ class uploadHomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //checks if all fields are filled in prior to sending to database
-    func checkFields() {
-        if (nameLabel.text != nil && addressLabel.text != nil && phoneNumberLabel.text != nil && homeTypeFromPicker.text != nil)
-        {
-            
-        }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton)
+    {
+        let backToHome = self.storyboard?.instantiateViewController(withIdentifier: "HomePageViewController") as! HomePageViewController
+            self.present(backToHome, animated: true)
     }
     
+    @IBAction func continueToPhotosBtn(_ sender: UIButton) {
+        
+        if(nameLabel.text == nil || addressLabel.text == nil || cityLabel.text == nil || stateLabel.text == nil || zipcodeLabel.text == nil || homeTypeFromPicker.text == nil)
+        {
+            firebaseReference?.child("Customers").childByAutoId().setValue(["Name": nameLabel.text,"Address": addressLabel.text,"City": cityLabel.text,"State": stateLabel.text,"Zipcode": zipcodeLabel.text, "PhoneNumber": phoneNumberLabel.text,"HouseType": homeTypeFromPicker.text])
+            
+            let toPhotoUpload = self.storyboard?.instantiateViewController(withIdentifier: "uploadPhotosView") as! uploadPhotosViewController
+            self.present(toPhotoUpload, animated: true)
+        }
+        else{
+             let alert = UIAlertController(title: "Whoops!", message: "You are missing fields.", preferredStyle: UIAlertControllerStyle.alert)
+            
+            alert.addAction(UIAlertAction(title:"Okay", style: .default, handler:  { action in self.performSegue(withIdentifier: "HomeUploadViewController", sender: self)}))
+            self.present(alert,animated: true, completion: nil)
+        }
+        
+    }
+    /////////////////////////////////////////////////////////
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+   //////////////////////////////////////////////////////////
 
 }
+
+
 
 extension uploadHomeViewController: UIPickerViewDelegate, UIPickerViewDataSource{
     
