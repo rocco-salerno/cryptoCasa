d66ba90300c08f6b8d8b7bd00454f8346e6a6627
carnage08 <rocco.m.salerno@gmail.com>
2018-09-21 01:42:05 -0400

    completed UIPicker

--- FinalProject/UploadHome/uploadHomeViewController.swift
+++ FinalProject/UploadHome/uploadHomeViewController.swift
@@ -14,12 +14,15 @@ class uploadHomeViewController: UIViewController {
     @IBOutlet weak var addressLabel: UITextField!
     @IBOutlet weak var phoneNumberLabel: UITextField!
     @IBOutlet weak var homeTypeOutlet: UIPickerView!
+    @IBOutlet weak var homeTypeFromPicker: UITextField!
     
-    let homeTypes = ["Ranch", "Split Level", "Victorian","Condo", "Duplex", "Penthouse"]
+    private let homeTypes = ["Ranch", "Split Level", "Victorian","Condo", "Duplex", "Penthouse"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        homeTypeOutlet.dataSource = self
+        homeTypeOutlet.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -27,13 +30,34 @@ class uploadHomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    //checks if all fields are filled in prior to sending to database
     func checkFields() {
-        if (nameLabel.text != nil && addressLabel.text != nil && phoneNumberLabel.text != nil)
+        if (nameLabel.text != nil && addressLabel.text != nil && phoneNumberLabel.text != nil && homeTypeFromPicker.text != nil)
         {
             
         }
     }
     
 
+}
+
+extension uploadHomeViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return homeTypes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        homeTypeFromPicker.text = homeTypes[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return homeTypes[row]
+    }
+    
 }
