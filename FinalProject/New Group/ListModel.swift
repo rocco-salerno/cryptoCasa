b8bf3dba0305e3da1ca2a9dcf44f61d4072ef226
carnage08 <rocco.m.salerno@gmail.com>
2018-11-29 02:27:34 -0500

    recieved child listings

---
+++ FinalProject/New Group/ListModel.swift
@@ -0,0 +1,14 @@
+//
+//  ListModel.swift
+//  Login_Register
+//
+//  Created by Rocco Salerno on 11/28/18.
+//  Copyright © 2018 Rocco Salerno. All rights reserved.
+//
+
+import UIKit
+
+class ListModel: NSObject {
+    var UID: String = ""
+    var PhotoURL: String = ""
+}
--- FinalProject/SearchHome/SearchHomeViewController.swift
+++ FinalProject/SearchHome/SearchHomeViewController.swift
@@ -10,49 +10,72 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class SearchHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SearchHomeViewController: UIViewController{
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var menuButtonOutlet: UIBarButtonItem!
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBAction func button(_ sender: UIButton) {
+        numberofElements.text = String(listingArray.count)
+    }
     
+    @IBOutlet weak var numberofElements: UILabel!
     var ref:  DatabaseReference?
     var databaseHandle: DatabaseHandle?
-    var listingsArray = [String]()
+    var ListArr = [ListModel]()
+    var listingArray = [String]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ref = Database.database().reference()
-        tableView.delegate = self
-        tableView.dataSource = self
+        //ref = Database.database().reference().child("Listings")
+//        tableView.delegate = self
+   //     tableView.dataSource = self
         //deleting all unused rows
-        tableView.tableFooterView = UIView(frame: CGRect.zero)
+      //  tableView.tableFooterView = UIView(frame: CGRect.zero)
 
-        databaseHandle = ref?.child("Listings").observe(.value, with: { (snapshot) in
+        //getting a snapshot of all children and their values
+        let ref = Database.database().reference().child("Listings")
+        ref.observe(.childAdded, with: { (snapshot) in
+            print(snapshot)
+            guard let dictionary = snapshot.value as? [String : AnyObject] else {
+                return
+            }
             
+            let Obj = ListModel()
+            Obj.UID = snapshot.key
+            Obj.PhotoURL = (dictionary["PhotoURL"] as? String)!
+            
+            self.labelOutlet.text = Obj.UID
+            
+            self.ListArr.append(Obj)
+            
+        }, withCancel: nil)
+        
+        
+        //getting number of listings for cell rows
+        databaseHandle = Database.database().reference().child("Listings").observe(.value, with: { (snapshot) in
             //try to convert our data to a String
             if let result = snapshot.children.allObjects as? [DataSnapshot] {
                 for child in result {
                     var userListings = child.key as! String
-                    //append data to myListings array
-                    self.listingsArray.append(userListings)
+                    //append data to listings array
+                    self.listingArray.append(userListings)
                     
                     //reload the tableview
-                    self.tableView.reloadData()
+                 //   self.tableView.reloadData()
                 }
             }})
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+ /*   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return listingsArray.count
+        return listingArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListingCell")
-        cell?.textLabel?.text = listingsArray[indexPath.row]
+        cell?.textLabel?.text = listingArray[indexPath.row]
         
         return cell!
     }
@@ -60,19 +83,5 @@ class SearchHomeViewController: UIViewController, UITableViewDelegate, UITableVi
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         
         return true
-    }
-    
-    func sideMenu()
-    {
-        if revealViewController != nil {
-            menuButtonOutlet.target = revealViewController()
-            menuButtonOutlet.action = #selector(SWRevealViewController.revealToggle(_:))
-            revealViewController()?.rearViewRevealWidth = 275
-            revealViewController()?.rightViewRevealWidth = 160
-            
-            view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
-            
-        }
-    }
-
+    } */
 }
